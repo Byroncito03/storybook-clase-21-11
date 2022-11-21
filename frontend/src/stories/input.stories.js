d16df3fadb3bb8cebbc63aedda28725e6151f625
Byroncito03 <113061489+Byroncito03@users.noreply.github.com>
2022-11-21 16:43:07 -0300

    first commit

---
+++ frontend/src/stories/input.stories.js
@@ -0,0 +1,16 @@
+import { Input } from "./Input";
+
+export default{
+    title:"Components/Input",
+    component:Input
+}
+
+const Template=(args)=><Input{...args}/>
+export const Default = Template.bind({})
+Default.args={}
+
+export const Large = Template.bind({})
+Large.args={
+    size:"large",
+    label:"Input"
+}
